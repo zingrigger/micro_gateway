7cb36e8dd734edb0615415d12a6a0e8e8882e39f
zingrigger <zingrigger@gmail.com>
2025-10-30 15:32:50 +0800

    feat(gateway): 补漏

--- src/main/java/com/bosyon/gateway/services/impl/TenantPublicKeyServiceImpl.java
+++ src/main/java/com/bosyon/gateway/services/impl/TenantPublicKeyServiceImpl.java
@@ -2,7 +2,9 @@ package com.bosyon.gateway.services.impl;
 
 import com.bosyon.gateway.model.TenantInfo;
 import com.bosyon.gateway.services.TenantPublicKeyService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TenantPublicKeyServiceImpl implements TenantPublicKeyService {
 
     @Override
